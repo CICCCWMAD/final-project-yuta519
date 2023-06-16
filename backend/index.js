3b5de73f0f836418d942c6415d49298407e5dbc5
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-06-17 08:52:34 +0900

    Load .env

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,32 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '.', '.env') });
+import path from'path';
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { createDbConnection } from './helpers/dbHelper.js'
+import authenticationRoute from './routes/authenticationRoutes.js';
+import chatRoomRoute from './routes/chatRoomRoutes.js'
+import chatRoute from './routes/chatRoutes.js';
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
-
-const authenticationRoute = require('./routes/authenticationRoutes');
-const chatRoomRoute = require('./routes/chatRoomRoutes');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-mongoose.connect('', {
-  useNewUrlParser: true,
-});
-
-const db = mongoose.connection;
+const db = createDbConnection(process.env.DB_URL)
 
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.error('connected to database'));
 
 app.use('/api/auth', authenticationRoute);
-app.use('/api/posts', chatRoomRoute);
+app.use('/api/chatrooms', chatRoomRoute);
+app.use('/api/chats', chatRoute);
+// app.use('/api/users', )
+// app.use('/api/auth', (req, res, next)=> res.send('hello'));
+// app.use('/api/posts', chatRoomRoute);
 
 app.listen(port, () => {
   console.log('Server is running');
